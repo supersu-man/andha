4651f0a33b44c57604ee74d4bc23234ffcafcce4
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2022-04-09 18:55:54 +0530

    other: First build

--- app/src/main/java/com/supersuman/andha/MainActivity.kt
+++ app/src/main/java/com/supersuman/andha/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.supersuman.andha
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.slider.Slider
+import com.google.android.material.textfield.TextInputEditText
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var editText : TextInputEditText
+    private lateinit var parentLayout : RelativeLayout
+    private lateinit var slider : Slider
+    private var myFontSize = 100f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        initViews()
+        modifyViews()
+        initListeners()
+
+
     }
+
+    private fun initViews() {
+        editText = findViewById(R.id.edit_text)
+        parentLayout = findViewById(R.id.parentLayout)
+        slider = findViewById(R.id.slider)
+    }
+
+    private fun modifyViews() {
+        setTextSize(myFontSize)
+    }
+
+    private fun initListeners(){
+        slider.addOnChangeListener { _, value, _ ->
+            setTextSize(value)
+        }
+    }
+
+    private fun setTextSize(f : Float){
+        editText.setTextSize(TypedValue.COMPLEX_UNIT_SP, f)
+    }
+
 }
